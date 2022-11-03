a7f6f289747e1881ba38128db3fd662eca6f0c5a
chrisneal72 <voodoodaddy2000@gmail.com>
2022-11-03 14:47:30 -0700

    Landing Page quick updates

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,27 +1,22 @@
 <template>
   <v-container class="home-view">
     <v-row class="text-center">
-      <v-col cols="12">
+      <!-- <v-col cols="12">
         <v-img
           :src="require('../assets/images/logo.svg')"
           class="my-3"
           contain
           height="200"
         />
-      </v-col>
+      </v-col> -->
 
       <v-col class="mb-4">
         <h1>
-          Welcome to Vuetify
+          Welcome to my Vue Portfolio
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          This page is just beginning. I am currently in the process of gathering some of my previous work to combine and showcase here. Including work I did on https://19crimes.com/pages/rewards and https://energizergorewards.com/
         </p>
       </v-col>
 
@@ -34,15 +29,7 @@
         </h2>
 
         <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
+          <p>I am woring on the Data Tab to add some better retrieval and diplay of the data coming from MongoDB. I am also going to make a section where new data can be added.</p>
         </v-row>
       </v-col>
 
@@ -66,27 +53,6 @@
           </a>
         </v-row>
       </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2>
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -100,53 +66,19 @@
 
     data: () => ({
       ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
       ],
       importantLinks: [
         {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
+          text: 'github',
+          href: 'https://github.com/chrisneal72',
         },
         {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
+          text: 'Energizer Go Rewards',
+          href: 'https://energizergorewards.com/',
         },
         {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+          text: '19 Crimes',
+          href: 'https://19crimes.com/pages/rewards',
         },
       ],
     }),
